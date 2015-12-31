83b7064279c207bba3295448d53e2804b4df5660
HEKET313 <neket313@gmail.com>
2015-12-31 12:54:25 +0300

    Слегка изменил фр. локализацию

--- wp-content/themes/main/localizations/locale_fr.php
+++ wp-content/themes/main/localizations/locale_fr.php
@@ -18,7 +18,7 @@ $local['site_name'] = 'Je parle russe';
 $local['footer']['languages'] = 'Langues';
 $local['footer']['address'] = 'Adresse';
 $local['footer']['street'] = 'Novoostapovskaya,';
-$local['footer']['home'] = 'Moscou, Russie, 12-15';
+$local['footer']['home'] = '12-15';
 $local['footer']['phone'] = 'Téléphone';
 $local['footer']['social'] = 'Réseaux sociaux';
 
