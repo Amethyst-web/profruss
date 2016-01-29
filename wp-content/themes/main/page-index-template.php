94d4e984d2a48c76103778526fed4944dea469dc
HEKET313 <neket313@gmail.com>
2016-01-29 09:17:50 +0300

    Поправил наезжание футера на контент на главной

--- wp-content/themes/main/page-index-template.php
+++ wp-content/themes/main/page-index-template.php
@@ -9,20 +9,22 @@
 
 get_header(); ?>
 
-<div class="container index">
-    <div class="content text-center">
-        <h1 class="green"><?=the_title();?></h1>
-        <?php if ( have_posts() ) : ?>
+<div class="wrapper">
+    <div class="container index">
+        <div class="content text-center">
+            <h1 class="green"><?=the_title();?></h1>
+            <?php if ( have_posts() ) : ?>
 
-            <?php while ( have_posts() ) : the_post(); ?>
-                <?php get_template_part( 'content', get_post_format() ); ?>
-            <?php endwhile; ?>
+                <?php while ( have_posts() ) : the_post(); ?>
+                    <?php get_template_part( 'content', get_post_format() ); ?>
+                <?php endwhile; ?>
 
-        <?php else : ?>
-            <?php get_template_part( 'content', 'none' ); ?>
-        <?php endif; ?>
+            <?php else : ?>
+                <?php get_template_part( 'content', 'none' ); ?>
+            <?php endif; ?>
 
-        <?php the_content();?>
+            <?php the_content();?>
+        </div>
     </div>
 </div>
 <?php get_footer(); ?>
