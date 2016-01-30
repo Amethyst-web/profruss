4c57b9da8c3785eed36e11e4112cee311a6aecbd
HEKET313 <neket313@gmail.com>
2016-01-30 17:10:12 +0300

    Сделал адаптивную шапку

--- wp-content/themes/main/head.php
+++ wp-content/themes/main/head.php
@@ -4,5 +4,7 @@
     <title><?php wp_title( '|', true, 'right' ); ?>Татьяна Федулова. Я говорю по-русски</title>
     <link rel="icon" type="image/png" href="/favicon.ico">
     <link rel="stylesheet" href="<?=get_template_directory_uri(); ?>/css/style.css" type="text/css" />
+    <script type="text/javascript" src="<?=get_template_directory_uri(); ?>/js/jquery-1.12.0.min.js"></script>
+    <script type="text/javascript" src="<?=get_template_directory_uri(); ?>/js/main.js"></script>
     <?php wp_head(); ?>
 </head>
---
+++ wp-content/themes/main/js/main.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Nikita on 30.01.2016.
+ */
+$(document).ready(function(){
+    setHeadPosition();
+    $( window ).resize(setHeadPosition);
+
+    function setHeadPosition(){
+        var headHeight = $(window).height() - 50;
+        if($(window).width() > headHeight) {
+            var titleMargin = (headHeight - 233) / 2;
+            $('header .container').css('min-height', headHeight);
+            $('header nav').css('top', headHeight/10);
+            if(titleMargin < 400) {
+                $('header .container .title').css('margin-top', titleMargin);
+            }
+        } else {
+            $('header .container, header .container .title, header nav').removeAttr('style');
+        }
+    }
+});
